8ff52a183825ac942b6caf285e000cf330cd66b5
N810-N <r.meron.810@outlook.jp>
2025-11-18 02:40:28 +0900

    がんばろうね！！

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show]
-  resources :blogs 
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
